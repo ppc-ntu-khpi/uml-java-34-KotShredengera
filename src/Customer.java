2cf43e4b09504ea0d935fd3996abeeafa10d715b
KotShredengera <61018452+KotShredengera@users.noreply.github.com>
2020-05-04 15:16:58 +0300

    Java class

---
+++ src/Customer.java
@@ -0,0 +1,242 @@
+package Customer;
+
+
+/**
+* @generated
+*/
+public class Customer {
+    
+    /**
+    * @generated
+    */
+    private Integer ID;
+    
+    /**
+    * @generated
+    */
+    private String Name;
+    
+    /**
+    * @generated
+    */
+    private String Address;
+    
+    /**
+    * @generated
+    */
+    private Integer PhoneNumber;
+    
+    /**
+    * @generated
+    */
+    private Integer AccountID;
+    
+    /**
+    * @generated
+    */
+    private String Email;
+    
+    
+    /**
+    * @generated
+    */
+    private Set<Account> account;
+    
+    /**
+    * @generated
+    */
+    private Set<Employee> employee;
+    
+    /**
+    * @generated
+    */
+    private Bank bank;
+    
+    /**
+    * @generated
+    */
+    private Set<Loan> loan;
+    
+    
+    /**
+    * @generated
+    */
+    public Integer getID() {
+        return this.ID;
+    }
+    
+    /**
+    * @generated
+    */
+    public Integer setID(Integer ID) {
+        this.ID = ID;
+    }
+    
+    /**
+    * @generated
+    */
+    public String getName() {
+        return this.Name;
+    }
+    
+    /**
+    * @generated
+    */
+    public String setName(String Name) {
+        this.Name = Name;
+    }
+    
+    /**
+    * @generated
+    */
+    public String getAddress() {
+        return this.Address;
+    }
+    
+    /**
+    * @generated
+    */
+    public String setAddress(String Address) {
+        this.Address = Address;
+    }
+    
+    /**
+    * @generated
+    */
+    public Integer getPhoneNumber() {
+        return this.PhoneNumber;
+    }
+    
+    /**
+    * @generated
+    */
+    public Integer setPhoneNumber(Integer PhoneNumber) {
+        this.PhoneNumber = PhoneNumber;
+    }
+    
+    /**
+    * @generated
+    */
+    public Integer getAccountID() {
+        return this.AccountID;
+    }
+    
+    /**
+    * @generated
+    */
+    public Integer setAccountID(Integer AccountID) {
+        this.AccountID = AccountID;
+    }
+    
+    /**
+    * @generated
+    */
+    public String getEmail() {
+        return this.Email;
+    }
+    
+    /**
+    * @generated
+    */
+    public String setEmail(String Email) {
+        this.Email = Email;
+    }
+    
+    
+    /**
+    * @generated
+    */
+    public Bank getBank() {
+        return this.bank;
+    }
+    
+    /**
+    * @generated
+    */
+    public Bank setBank(Bank bank) {
+        this.bank = bank;
+    }
+    
+    /**
+    * @generated
+    */
+    public Loan getLoan() {
+        return this.loan;
+    }
+    
+    /**
+    * @generated
+    */
+    public Loan setLoan(Loan loan) {
+        this.loan = loan;
+    }
+    
+    /**
+    * @generated
+    */
+    public Account getAccount() {
+        return this.account;
+    }
+    
+    /**
+    * @generated
+    */
+    public Account setAccount(Account account) {
+        this.account = account;
+    }
+    
+    /**
+    * @generated
+    */
+    public Employee getEmployee() {
+        return this.employee;
+    }
+    
+    /**
+    * @generated
+    */
+    public Employee setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+    
+
+    //                          Operations                                  
+    
+    /**
+    * @generated
+    */
+    public DepositMoney() {
+        //TODO
+    }
+    /**
+    * @generated
+    */
+    public WithdrawMoney() {
+        //TODO
+    }
+    /**
+    * @generated
+    */
+    public OpenAccount() {
+        //TODO
+    }
+    /**
+    * @generated
+    */
+    public CloseAccount() {
+        //TODO
+    }
+    /**
+    * @generated
+    */
+    public BorrowLoan() {
+        //TODO
+    }
+    /**
+    * @generated
+    */
+    public PayEmission() {
+        //TODO
+    }
+    
+}
